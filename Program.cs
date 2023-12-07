e9d06217265fa4c4f270114222fe625bad50a422
boris Ningthoujam <borisningthoujam@gmail.com>
2023-12-07 15:33:51 +0530

    latest

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.EntityFrameworkCore;
 using PlatformService.AsyncDataServices;
 using PlatformService.Data;
@@ -9,17 +10,27 @@ using PlatformService.SyncDataServices.HttpGet;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.ConfigureKestrel(options =>
+{
+    //Setup a HTTP/2 endpoint without TLS.
+    options.ListenAnyIP(666, o => o.Protocols =
+        HttpProtocols.Http2);
+        options.ListenAnyIP(777, o => o.Protocols =
+        HttpProtocols.Http1);
+
+});
+
 //Add services to the container.
-if (builder.Environment.IsProduction())
-{
-    builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("platformdb")));
-    Console.WriteLine("Using Sql Server");
-}
-else
-{
-     builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMemory"));
+// if (builder.Environment.IsProduction())
+// {
+//     builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("platformdb")));
+//     Console.WriteLine("Using Sql Server");
+// }
+// else
+// {
+    builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMemory"));
     Console.WriteLine("Using InMemory Database");
-}
+//}
 // builder.Services.AddHostedService<MessageBusSubscriber>();
 
 builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
@@ -27,7 +38,7 @@ builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 builder.Services.AddSingleton<IMessageBusCLient, MessageBusClient>();
 // builder.Services.AddSingleton<IMessageProducer,RabbitMQProducer>();
 
-builder.Services.AddScoped<IPlatformRepo,PlatformRepo>();
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 
 builder.Services.AddGrpc();
 builder.Services.AddControllers();
@@ -42,20 +53,20 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 // if (app.Environment.IsDevelopment())
 // {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 //}
 
 //app.UseHttpsRedirection();
 
-await SeedData.PrePopulateAsync(app,app.Environment.IsProduction());
+//await SeedData.PrePopulateAsync(app, app.Environment.IsProduction());
 
 //app.UseAuthorization();
 
 app.MapControllers();
 app.MapGrpcService<GrpcPlatformService>();
-app.MapGet("/protos/platform.proto", async  context=>
+app.MapGet("/protos/platform.proto", async context =>
 {
- await context.Response.WriteAsync(File.ReadAllText("protos/platforms.proto"));
+    await context.Response.WriteAsync(File.ReadAllText("protos/platform.proto"));
 });
 app.Run();
